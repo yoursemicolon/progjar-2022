893d9c576c23a9873b1042db8021769e4bf7bc1a
Amanda Rozi Kurnia <amandarozikurnia@gmail.com>
2022-06-10 13:20:01 +0000

    refactor: move lower function position

--- progjar4a/tugas4a/file_protocol.py
+++ progjar4a/tugas4a/file_protocol.py
@@ -23,9 +23,9 @@ class FileProtocol:
         self.file = FileInterface()
     def proses_string(self,string_datamasuk=''):
         logging.warning(f"string diproses: {string_datamasuk}")
-        c = shlex.split(string_datamasuk.lower())
+        c = shlex.split(string_datamasuk)
         try:
-            c_request = c[0].strip()
+            c_request = c[0].lower().strip()
             logging.warning(f"memproses request: {c_request}")
             params = [x for x in c[1:]]
             cl = getattr(self.file,c_request)(params)
